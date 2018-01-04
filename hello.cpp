57aa30c364bbc72596fdb142cc5ee6f68d1b295c
Fred Kerr <fekerr@gmail.com>
2018-01-03 18:58:11 -0800

    Add files via upload
    I should make a real makefile instead of make.sh :)

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// guess what this is.
+// build with "g++ -Wall -g hello.cpp"
+// or something.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hey, world" << endl;
+    return 0;
+}
---
+++ make.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+g++ -Wall -g hello.cpp
+
